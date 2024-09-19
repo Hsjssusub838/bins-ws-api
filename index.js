ac82dbc5492c5bc11ee6588e3da841c56efdafc5
Hsjssusub838 <pro4303421421@gmail.com>
2024-09-19 11:17:42 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-// @ts-check
 const express = require("express");
 const { binLookup } = require("@arnabxd/bin-lookup");
 
 const app = express();
+const port = process.env.PORT || 4000;
 
-const PORT = process.env.PORT || 3000;
+// Route for the root
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 
+// Route for /api/:bin
 app.get("/api/:bin", async (req, res) => {
   let data = await binLookup(req.params.bin, "bins.ws");
   res.set("Cache-Control", "public, max-age=86400");
@@ -13,14 +17,14 @@ app.get("/api/:bin", async (req, res) => {
   res.send(data);
 });
 
-app.use(async (_, res) => {
+// Redirect any other route to the GitHub repository
+app.use((_, res) => {
   res.redirect(301, "https://github.com/ArnabXD/bins-ws-api");
 });
 
-if (process.env.NODE_ENV !== "production") {
-  app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
-  });
-}
+// Start the server
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Example app listening on port ${port}`);
+});
 
 module.exports = app;
